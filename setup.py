5dfd7f0ba8ce96efe824d0d703bcd1373c36ad4f
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2017-06-12 21:28:13 -0500

    Jumpstart the library.

---
+++ setup.py
@@ -0,0 +1,23 @@
+"""
+Setup of the wostools project.
+"""
+
+from setuptools import find_packages
+from setuptools import setup
+
+VERSION = '0.1.0'
+
+setup(
+    name='wostools',
+    version=VERSION,
+    author='Oscar D. Arbeláez-Echeverri <@odarbelaeze>, Juan C. Henao-Londoño',
+    author_email='odarbelaeze@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    tests_require=[],
+    setup_requires=[],
+    url='https://github.com/pcm-ca/wostools',
+    download_url=f'https://github.com/pcm-ca/wostools/tarball/{VERSION}',
+    keywords=['wos', 'bibliography', ],
+    description='Utilities for the wos plain text files',
+)
---
+++ wostools.py
@@ -0,0 +1,141 @@
+"""
+The whole wostools thing.
+"""
+
+import collections
+import itertools
+import re
+
+
+def popular(iterable, limit):
+    """
+    A little utility to compute popular values on an iterable.
+    """
+    return collections.Counter(iterable).most_common(limit)
+
+
+def article_text_to_dict(article_text: str):
+    """
+    Translates an article text into a dict.
+    """
+    data = collections.defaultdict(list)
+    field = ''
+    for line in re.split(r'\n+', article_text):
+        # Fix little bug with isi files
+        if line.startswith('null'):
+            line = line[4:]
+        name = line[:2]
+        value = line[3:]
+        if not name.isspace():
+            field = name
+        if not field.isspace() and field != 'ER':
+            data[field].append(value)
+    return dict(data)
+
+
+class WosToolsError(Exception):
+    """
+    All the errors go here.
+    """
+    pass
+
+
+class Article(object):
+    """
+    Abstract a WOS article.
+    """
+
+    def __init__(self, article_text):
+        self._article_text = article_text
+        self._data = article_text_to_dict(article_text)
+
+    def __getattr__(self, name):
+        if name not in self._data and not hasattr(self._data, name):
+            raise AttributeError(f'{self.__class__.__name__} does not have an attribute {name}')
+        if name not in self._data:
+            return getattr(self._data, name)
+        return self._data[name]
+
+    def __hasattr__(self, name):
+        return name in self._data
+
+
+class CollectionLazy(object):
+    """
+    A collection of WOS text files.
+    """
+
+    def __init__(self, *filenames):
+        self.filenames = filenames
+
+
+    @property
+    def files(self):
+        """
+        Iterates over all files in the collection
+        """
+        for filename in self.filenames:
+            try:
+                with open(filename) as filehandle:
+                    yield filehandle
+            except FileNotFoundError:
+                raise WosToolsError(f'The file {filename} was not found')
+
+    @property
+    def article_texts(self):
+        """
+        Iterates over all the single article texts in the colection.
+        """
+        for filehandle in self.files:
+            data = filehandle.read()
+            for article_text in data.split('\n\n')[1:]:
+                if article_text == 'EF':
+                    continue
+                yield article_text
+
+    @property
+    def articles(self):
+        """
+        Iterates over all articles.
+        """
+        for article_text in self.article_texts:
+            yield Article(article_text)
+
+    @property
+    def authors(self):
+        """
+        Iterates over all article authors, including duplicates
+        """
+        authors = (
+            article.AF
+            for article in self.articles
+            if hasattr(article, 'AF')
+        )
+        return itertools.chain(*authors)
+
+    @property
+    def coauthors(self):
+        """
+        Iterates over coauthor pairs.
+        """
+        authors_by_article = (
+            article.AF
+            for article in self.articles
+            if hasattr(article, 'AF')
+        )
+        return itertools.chain(*(
+            itertools.combinations(sorted(authors), 2)
+            for authors in authors_by_article
+        ))
+
+    def completeness(self, key=None):
+        """
+        Computes the completeness of the collection by key.
+        """
+        counters = collections.defaultdict(int)
+        total = 0
+        for article in self.articles:
+            total += 1
+            for key in article.keys():
+                counters[key] += 1
+        return {key: val/total for key, val in counters.items()}

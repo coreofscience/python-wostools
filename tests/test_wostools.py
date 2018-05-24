d78c811e2e55f6a85015101745d8f3f8868c0dfe
dsvalenciah <dsvalenciah@unal.edu.co>
2018-05-24 16:53:46 -0500

    Add Article label test.

--- tests/test_wostools.py
+++ tests/test_wostools.py
@@ -3,7 +3,6 @@
 
 """Tests for `wostools` package."""
 
-
 from wostools import CollectionLazy
 
 
@@ -14,3 +13,13 @@ def test_collection():
     collection = CollectionLazy('docs/examples/bit-pattern-savedrecs.txt')
     for article in collection.articles:
         assert article.TI
+
+
+def test_article_label(article):
+    """
+    Test label value of article.
+    """
+    assert article.label == (
+        'Wodarz S, 2017, JOURNAL OF MAGNETISM AND MAGNETIC MATERIALS, '
+        'V430, P7, DOI 10.1016/j.jmmm.2017.01.061'
+    )

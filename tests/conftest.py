7a9df966faf0dd0abfc26d8395cc88b6ba5155d2
dsvalenciah <dsvalenciah@unal.edu.co>
2018-05-24 16:56:58 -0500

    Create article fixture.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,4 +2,13 @@
 Configuration file for python-wostools tests.
 """
 
+from wostools import Article
+
 import pytest
+
+
+@pytest.fixture()
+def article():
+    article_file = open('docs/examples/bit-pattern-article-savedrecs.txt')
+    article_text = article_file.read()
+    return Article(article_text)

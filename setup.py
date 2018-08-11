6e42377695a099c592f64ff2f8e1a7104f3e93ae
Oscar David Arbeláez <odarbelaeze@gmail.com>
2018-08-11 13:47:18 -0500

    Add networkx as a dependency

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = ['networkx>=2.0,<3.0']
 
 setup_requirements = ['pytest-runner', ]
 

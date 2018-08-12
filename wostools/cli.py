dfd646e0cce3720cd3b9e40e60c5811e65131f10
Oscar David Arbeláez <oscar.arbelaez@adroll.com>
2018-08-12 13:04:01 -0500

    Add citation graph command

--- wostools/cli.py
+++ wostools/cli.py
@@ -1,5 +1,9 @@
 import click
 
+import networkx
+
+from wostools import CollectionLazy
+
 
 @click.group()
 def main():
@@ -12,6 +16,14 @@ def main():
 @main.command('citation-graph')
 @click.argument('source', type=click.File('r'), nargs=-1)
 @click.argument('output', type=click.File('w'), nargs=1)
-def citation_graph(source, target):
-    pass
-
+def citation_graph(source, output):
+    """
+    Build a citation graph.
+    """
+    collection = CollectionLazy(*[f.name for f in source])
+    graph = collection.to_graph()
+    networkx.write_graphml(graph, output.name)
+    click.echo(click.style(
+        f'Graph successfuly written to {output.name}',
+        fg='green'
+    ))

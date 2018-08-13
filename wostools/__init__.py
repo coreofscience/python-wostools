206ec10269b6667bf8116f266b14a3df71b29bb8
Oscar David Arbeláez <oscar.arbelaez@adroll.com>
2018-08-12 19:17:11 -0500

    Bump version: 0.1.1 → 0.2.0

--- wostools/__init__.py
+++ wostools/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Core of Science"""
 __email__ = 'dev@coreofscience.com'
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 
 from wostools.wostools import CollectionLazy, WosToolsError, Article
 

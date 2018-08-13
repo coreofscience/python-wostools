ac60773bc030cbe1d21d14c24f8d517e4e12b767
Oscar David Arbeláez <odarbelaeze@gmail.com>
2018-08-12 23:57:23 -0500

    Bump version: 0.2.2 → 0.2.3

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/coreofscience/python-wostools',
-    version='0.2.2',
+    version='0.2.3',
     zip_safe=False,
 )
--- wostools/__init__.py
+++ wostools/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Core of Science"""
 __email__ = 'dev@coreofscience.com'
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 from wostools.wostools import CollectionLazy, WosToolsError, Article
 

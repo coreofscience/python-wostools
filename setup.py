48e0cda7c1f249cad071e4f29517d47d1da31356
Oscar David Arbeláez <odarbelaeze@gmail.com>
2018-08-12 23:49:40 -0500

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/coreofscience/python-wostools',
-    version='0.2.1',
+    version='0.2.2',
     zip_safe=False,
 )

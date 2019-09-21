b76b2bc8ef2584bf01b0ccea1e25448b9511e928
Juan David <jdalzatec@unal.edu.co>
2019-09-21 16:43:44 -0500

    Change click to Click requirement in the setup.py file

--- setup.py
+++ setup.py
@@ -4,46 +4,42 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['networkx>=2.0,<3.0', 'click>=6.7,<7.0']
+requirements = ["networkx>=2.0,<3.0", "Click>=6.7,<=7.0"]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = ["pytest-runner"]
 
-test_requirements = ['pytest', ]
+test_requirements = ["pytest"]
 
 setup(
     author="Core of Science",
-    author_email='dev@coreofscience.com',
+    author_email="dev@coreofscience.com",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
     ],
-    entry_points={
-        'console_scripts': [
-            'wostools=wostools.cli:main',
-        ],
-    },
+    entry_points={"console_scripts": ["wostools=wostools.cli:main"]},
     description="Translates isi web of knowledge files into python objects.",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='wostools',
-    name='wostools',
-    packages=find_packages(include=['wostools']),
+    keywords="wostools",
+    name="wostools",
+    packages=find_packages(include=["wostools"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/coreofscience/python-wostools',
-    version='0.2.3',
+    url="https://github.com/coreofscience/python-wostools",
+    version="0.2.3",
     zip_safe=False,
 )

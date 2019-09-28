4d63035e657bf9c36e00e45cf27bbb0dbce17eec
Juan David <jdalzatec@unal.edu.co>
2019-09-28 18:03:11 -0500

    Reformat article_text_to_dict

--- wostools/wostools.py
+++ wostools/wostools.py
@@ -23,17 +23,20 @@ def article_text_to_dict(article_text: str):
     """
     Translates an article text into a dict.
     """
+    # Fix little bug with isi files
+    if article_text.startswith("null"):
+        article_text = article_text[4:]
+
     data = collections.defaultdict(list)
-    field = ''
-    for line in re.split(r'\n+', article_text):
-        # Fix little bug with isi files
-        if line.startswith('null'):
-            line = line[4:]
+    field = ""
+    for line in re.split(r"\n+", article_text):
         name = line[:2]
         value = line[3:]
+
         if not name.isspace():
             field = name
-        if not field.isspace() and field != 'ER':
+
+        if field != "ER":
             data[field].append(value)
     return dict(data)
 
@@ -42,6 +45,7 @@ class WosToolsError(Exception):
     """
     All the errors go here.
     """
+
     pass
 
 
@@ -58,7 +62,7 @@ class Article(object):
     def __getattr__(self, name):
         if name not in self._processed_data:
             raise AttributeError(
-                f'{self.__class__.__name__} does not have an attribute {name}'
+                f"{self.__class__.__name__} does not have an attribute {name}"
             )
         return self._processed_data[name]
 
@@ -68,19 +72,19 @@ class Article(object):
     @property
     def label(self):
         fields_normalizers = {
-            'AU': lambda au: au[0].replace(',', ''),
-            'PY': lambda py: py[0],
-            'J9': lambda j9: j9[0],
-            'VL': lambda vl: f'V{vl[0]}',
-            'PG': lambda pg: f'P{pg[0]}',
-            'DI': lambda di: f'DOI {di[0]}',
+            "AU": lambda au: au[0].replace(",", ""),
+            "PY": lambda py: py[0],
+            "J9": lambda j9: j9[0],
+            "VL": lambda vl: f"V{vl[0]}",
+            "PG": lambda pg: f"P{pg[0]}",
+            "DI": lambda di: f"DOI {di[0]}",
         }
         normalized_fields = [
             normalizer(self._data[field])
             for field, normalizer in fields_normalizers.items()
             if self._data.get(field)
         ]
-        label = ', '.join(normalized_fields)
+        label = ", ".join(normalized_fields)
         return label
 
 
@@ -109,7 +113,7 @@ class CollectionLazy(object):
                 with open(filename) as filehandle:
                     yield filehandle
             except FileNotFoundError:
-                raise WosToolsError(f'The file {filename} was not found')
+                raise WosToolsError(f"The file {filename} was not found")
 
     @property
     def article_texts(self):
@@ -118,8 +122,8 @@ class CollectionLazy(object):
         """
         for filehandle in self.files:
             data = filehandle.read()
-            for article_text in data.split('\n\n')[1:]:
-                if article_text == 'EF':
+            for article_text in data.split("\n\n")[1:]:
+                if article_text == "EF":
                     continue
                 yield article_text
 
@@ -136,11 +140,7 @@ class CollectionLazy(object):
         """
         Iterates over all article authors, including duplicates
         """
-        authors = (
-            article.AF
-            for article in self.articles
-            if hasattr(article, 'AF')
-        )
+        authors = (article.AF for article in self.articles if hasattr(article, "AF"))
         return itertools.chain(*authors)
 
     @property
@@ -149,14 +149,14 @@ class CollectionLazy(object):
         Iterates over coauthor pairs.
         """
         authors_by_article = (
-            article.AF
-            for article in self.articles
-            if hasattr(article, 'AF')
+            article.AF for article in self.articles if hasattr(article, "AF")
         )
-        return itertools.chain(*(
-            itertools.combinations(sorted(authors), 2)
-            for authors in authors_by_article
-        ))
+        return itertools.chain(
+            *(
+                itertools.combinations(sorted(authors), 2)
+                for authors in authors_by_article
+            )
+        )
 
     def completeness(self, key=None):
         """
@@ -168,7 +168,7 @@ class CollectionLazy(object):
             total += 1
             for key in article.keys():
                 counters[key] += 1
-        return {key: val/total for key, val in counters.items()}
+        return {key: val / total for key, val in counters.items()}
 
     def to_graph(self):
         adjacency = [
@@ -180,11 +180,11 @@ class CollectionLazy(object):
         g.add_edges_from(adjacency)
         for alias in field_aliases():
             attributes = {
-                article.label: article._processed_data.get(alias, '')
+                article.label: article._processed_data.get(alias, "")
                 for article in self.articles
             }
             attributes = {
-                label: ';'.join(value) if isinstance(value, list) else value
+                label: ";".join(value) if isinstance(value, list) else value
                 for label, value in attributes.items()
             }
             nx.set_node_attributes(g, attributes, alias)

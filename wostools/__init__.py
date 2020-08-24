4b962bba5ee3389117eca15ddb34511c36616af9
Oscar Arbeláez <odarbelaeze@gmail.com>
2020-08-24 18:28:50 -0500

    Bump version

--- wostools/__init__.py
+++ wostools/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Core of Science"""
 __email__ = "dev@coreofscience.com"
-__version__ = "2.0.6"
+__version__ = "2.0.7"
 
 from wostools.article import Article
 from wostools.lazy import LazyCollection
